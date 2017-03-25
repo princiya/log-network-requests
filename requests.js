e3c6cd2948cce38c532bdfcf45e9866167affcbc
princiya <princiya777@yahoo.co.in>
2017-03-25 14:04:58 +0100

    filter 3rd party requests

--- requests.js
+++ requests.js
@@ -1,8 +1,29 @@
+var pattern = null;
+
+function getHostname(url) {
+	let urlArray = url.split('/');
+	return urlArray[2];
+}
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+  if (changeInfo.url) {
+    pattern = getHostname(changeInfo.url);
+  }
+}
+
+browser.tabs.onUpdated.addListener(handleUpdated);
+
 function logURL(requestDetails) {
-  console.log("Loading: " + requestDetails.url);
+  let url = requestDetails.url;
+
+  if (pattern &&
+  	/^http.*/.test(url) &&	 
+  	!pattern.match(getHostname(url))) {
+  		console.log(" Loading 3rd party: " + url);
+  }
 }
 
 browser.webRequest.onBeforeRequest.addListener(
   logURL,
-  {urls: ["<all_urls>"]}
+  {urls: [ "<all_urls>"]}
 );
